b4ec7dc7a73a4e53f405523d4003d5037db92286
Hector Mario Carvajal de los Reyes <hectordico@hotmail.com>
2026-04-22 17:39:34 -0500

    cambios

--- protafolio/script.js
+++ protafolio/script.js
@@ -1,22 +1,32 @@
-window.addEventListener("load", () => {
-    alert("WELCOME");
+window.addEventListener("load", function(){
+alert("WELCOME");
 });
 
 
-const boton = document.getElementById("btn-imagen");
+/* MOSTRAR / OCULTAR IMAGEN */
+const btnImagen = document.getElementById("btn-imagen");
 const imagen = document.getElementById("imagen");
 
-if (boton && imagen) {
+btnImagen.addEventListener("click", function(){
 
-    boton.addEventListener("click", () => {
-
-        if (imagen.style.display === "none") {
-            imagen.style.display = "block";
-        } else {
-            imagen.style.display = "none";
-        }
-
-    });
-
+if(imagen.style.display === "none"){
+imagen.style.display = "block";
+}else{
+imagen.style.display = "none";
 }
 
+});
+
+
+const boton = document.getElementById("boton");
+const texto = document.getElementById("texto");
+
+boton.addEventListener("click", function(){
+
+if(texto.textContent.trim() === "Allow yourself to dream."){
+texto.textContent = "Work for the now.";
+}else{
+texto.textContent = "Allow yourself to dream.";
+}
+
+});
